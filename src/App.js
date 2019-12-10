9ef43df20bb2aa71c0f48cf98a280e9ee1390997
Clara <claragubau@gmail.com>
2019-12-10 18:28:53 +0100

    equipo fet

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {Route, Switch, BrowserRouter} from 'react-router-dom'
 const Equipo = React.lazy(() => import('./Components/Equipo/Equipo'));
 const Colabora = React.lazy(() => import('./Components/Colabora/Colabora')); 
 const Home = React.lazy(() => import('./Components/Home/Home'));
-const Prova = React.lazy(() => import('./Components/Prova/Prova'));
 const Proyecto = React.lazy(() => import('./Components/Proyecto/Proyecto'));
 const Revista = React.lazy(() => import('./Components/Revista/Revista'))
 const NavBar = React.lazy(() => import('./Components/NavBar/NavBar'))
@@ -25,32 +24,11 @@ function App() {
       <Route path="/colabora" exact component={Colabora}/>
       <Route path="/revista" exact component={Revista}/>
       <Route path="/proyecto" exact component={Proyecto}/>
-      <Route path="/prova" exact component = {Prova}/>
       </Switch>
       </BrowserRouter>
     </div>
     </React.Suspense>
   );
-}
-
-/* <div id="myNav" class="overlay">
-<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-<div class="overlay-content">
-  <a href="#">About</a>
-  <a href="#">Services</a>
-    <a href="#">Clients</a>
-    <a href="#">Contact</a>
-</div>
-</div>
-
-<span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; open</span> */
-function openNav() {
-  document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
 }
 
 export default App;
--- src/Components/Equipo/Equipo.js
+++ src/Components/Equipo/Equipo.js
@@ -10,9 +10,9 @@ import './equipo.css'
 function Equipo(){
     return(
         <div>
-            {/* <h1 className="equipo1">EQUIPO</h1>
+            <h1 className="equipo1">EQUIPO</h1>
             <h2 className="equipo">OASIS</h2>
-            <div className="rectequipo"></div> */}
+            <div className="rectequipo"></div>
             <div className="grid-container">
                 <div className="column">
                     <div className="row">
@@ -56,13 +56,72 @@ function Equipo(){
             </div>
 
             <div className="backequipo">
-                Dirección y Guión
-                DAVID ALSINA
-                Ayudante de Dirección
-                LLUÍS CASCALES
-                Auxiliar de Ayudante de Dirección
-                BERTA VILARÓ
-                
+                <div className="grid">
+                    <div className="column1">
+                        <div className="first">Dirección y Guión</div>
+                        <div className="second">DAVID ALSINA</div>
+                        <div className="first">Ayudante de Dirección</div>
+                        <div className="second">LLUÍS CASCALES</div>
+                        <div className="first">Auxiliar de Ayudante de Dirección</div>
+                        <div className="second">BERTA VILARÓ</div>
+                        <div className="first">Script</div>
+                        <div className="second">JÚLIA RECARENS</div>
+                        <div className="first">Dirección de Arte y Diseño Gráfico</div>
+                        <div className="second">MARIA BOQUERA</div>
+                        <div className="first">Ayudante de Arte</div>
+                        <div className="second">VÍCTOR GARCIA</div>
+                        <div className="first">Auxiliares de Arte</div>
+                        <div className="second">CLARA BORRULL<br/>MÒNICA FORNELLS</div>
+                        <div className="first">Maquillaje</div>
+                        <div className="second">LAIA COMELLA</div>
+                        <div className="first">Vestuario</div>
+                        <div className="second">CLARA BORRULL</div>
+                        <div className="first">Programación</div>
+                        <div className="second">CLARA GUBAU</div>
+                   </div>
+                   <div className="column2">
+                       <div className="first">Producción</div>
+                       <div className="second">MARTA CÁMARA</div>
+                       <div className="first">Auxiliar de Producción</div>
+                       <div className="second">YERAY CORONADO</div>
+                       <div className="first">Técnica de Sonido</div>
+                       <div className="second">ISABEL PÉREZ</div>
+                       <div className="first">Técnico de Sonido</div>
+                       <div className="second">HANS LUDWIG</div>
+                       <div className="first">Ayudantes de Sonido</div>    
+                       <div className="second">ROC FAIXEDAS<br/>LLÍVIA FULQUET</div>
+                       <div className="first">Script de Sonido</div>
+                       <div className="second">LÍVIA FULQUET</div>
+                       <div className="first">Compositor BSO</div>
+                       <div className="second">PABLO RUIZ</div>
+                       <div className="first">Editor de Sonido</div>
+                       <div className="second">LLUÍS CASCALES<br/>HANS LUDWIG</div> 
+                       <div className="first">Reparto</div>
+                        <div className="second">MIREIA FERRÉ<br/>DESCONOCIDO<br/>DESCONOCIDO</div>
+                    </div> 
+                    <div className="column3">
+                    <div className="first">Dirección de Fotografia y Primer operador de Cámara</div>
+                    <div className="second">PAU SIMON</div>
+                    <div className="first">Ayudante de Cámara</div>
+                    <div className="second">ÈRIC MARCO</div>
+                    <div className="first">Auxiliar de Cámara</div>
+                    <div className="second">ADAM PUIG</div>
+                    <div className="first">Forquista</div>
+                    <div className="second">SAIOA ARROYO</div>
+                    <div className="first">Gaffer</div>
+                    <div className="second">desconocido</div>
+                    <div className="first">Eléctricos</div>
+                    <div className="second">RICARD PALET</div>
+                    <div className="first">Dirección de Post-producción</div>
+                    <div className="second">ISABEL PÉREZ</div>
+                    <div className="first">Ayudante de Post-producción</div>
+                    <div className="second">DESCONOCIDO</div>
+                    <div className="first">Foto fija</div>
+                    <div className="second">MARTÍ MENENDEZ</div>
+                    <div className="first">Making of</div>
+                    <div className="second">PAU POMAR</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
