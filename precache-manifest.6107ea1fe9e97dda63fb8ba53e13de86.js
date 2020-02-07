cd199c76f3a08ab27350c1970b23d52e3689d160
adriacabeza <adriacabeza@hotmail.com>
2020-02-07 13:12:27 +0100

    Updates

---
+++ precache-manifest.6107ea1fe9e97dda63fb8ba53e13de86.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfdf003e761c527467ef00cfb18ec42",
+    "url": "/oasis/index.html"
+  },
+  {
+    "revision": "59d7688f3877e287c908",
+    "url": "/oasis/static/css/3.19bac989.chunk.css"
+  },
+  {
+    "revision": "d2575bd8d8d2ecdf5fb4",
+    "url": "/oasis/static/css/4.635c143d.chunk.css"
+  },
+  {
+    "revision": "7d4ba6c8d2b66ad35a46",
+    "url": "/oasis/static/css/5.c81afef7.chunk.css"
+  },
+  {
+    "revision": "df35cc5520c6f9a48f93",
+    "url": "/oasis/static/css/6.537adc8f.chunk.css"
+  },
+  {
+    "revision": "98be6bb08cb28c908e4b",
+    "url": "/oasis/static/css/7.6c768c0b.chunk.css"
+  },
+  {
+    "revision": "3f35a937a2a37facc505",
+    "url": "/oasis/static/css/8.b742c376.chunk.css"
+  },
+  {
+    "revision": "68b1ffb76a239ecc781e",
+    "url": "/oasis/static/css/9.faf38ff0.chunk.css"
+  },
+  {
+    "revision": "00e21097c459add6ae59",
+    "url": "/oasis/static/css/main.9ea0a72c.chunk.css"
+  },
+  {
+    "revision": "6bc229f32bc91ffdc7cc",
+    "url": "/oasis/static/js/2.00eb1abb.chunk.js"
+  },
+  {
+    "revision": "59d7688f3877e287c908",
+    "url": "/oasis/static/js/3.5d9fe349.chunk.js"
+  },
+  {
+    "revision": "d2575bd8d8d2ecdf5fb4",
+    "url": "/oasis/static/js/4.317cabfb.chunk.js"
+  },
+  {
+    "revision": "7d4ba6c8d2b66ad35a46",
+    "url": "/oasis/static/js/5.c743caf5.chunk.js"
+  },
+  {
+    "revision": "df35cc5520c6f9a48f93",
+    "url": "/oasis/static/js/6.09a8fa36.chunk.js"
+  },
+  {
+    "revision": "98be6bb08cb28c908e4b",
+    "url": "/oasis/static/js/7.d7c35d5e.chunk.js"
+  },
+  {
+    "revision": "3f35a937a2a37facc505",
+    "url": "/oasis/static/js/8.1953745a.chunk.js"
+  },
+  {
+    "revision": "68b1ffb76a239ecc781e",
+    "url": "/oasis/static/js/9.3ea25927.chunk.js"
+  },
+  {
+    "revision": "00e21097c459add6ae59",
+    "url": "/oasis/static/js/main.63e10318.chunk.js"
+  },
+  {
+    "revision": "af6b013b93b97daa8d5f",
+    "url": "/oasis/static/js/runtime-main.f87f2451.js"
+  },
+  {
+    "revision": "662c9dfa6ac035f8c97ccc2a901f5e32",
+    "url": "/oasis/static/media/ACTORS.662c9dfa.jpg"
+  },
+  {
+    "revision": "e5d847d12cbc180adaca8b5e212cc925",
+    "url": "/oasis/static/media/Asap-Regular.e5d847d1.ttf"
+  },
+  {
+    "revision": "149b084d9734e4dc3d46a187c5f97685",
+    "url": "/oasis/static/media/HABADA.149b084d.jpg"
+  },
+  {
+    "revision": "3d1991bdeea4632b7ce9b5b6930b1a7c",
+    "url": "/oasis/static/media/Pattern_Verd.3d1991bd.jpg"
+  },
+  {
+    "revision": "8ebc6bc17bfd36f5e9a1be152f7ae553",
+    "url": "/oasis/static/media/bequipo.8ebc6bc1.gif"
+  },
+  {
+    "revision": "18b401cfe8167b96c6ba3aafd4d48e09",
+    "url": "/oasis/static/media/buke.18b401cf.gif"
+  },
+  {
+    "revision": "d159feabaf3a34cbb7f5e02969c85d82",
+    "url": "/oasis/static/media/da.d159feab.gif"
+  },
+  {
+    "revision": "5d2aa075ba11e140254bca8ba1e4c64b",
+    "url": "/oasis/static/media/fcolaborafinal.5d2aa075.jpg"
+  },
+  {
+    "revision": "ac03c0d1b54e6d7419d86d1184831789",
+    "url": "/oasis/static/media/fmenu.ac03c0d1.jpg"
+  },
+  {
+    "revision": "c753a04e105c99f90273e4ec5ee1a351",
+    "url": "/oasis/static/media/grevista.c753a04e.gif"
+  },
+  {
+    "revision": "f8cb21a6e8f539492677144040759e54",
+    "url": "/oasis/static/media/isi.f8cb21a6.gif"
+  },
+  {
+    "revision": "432a073c3ea30a9f5e1bca04bd321e25",
+    "url": "/oasis/static/media/lluis.432a073c.gif"
+  },
+  {
+    "revision": "e371ea5d2cde06e4cd1786e0c4528f3e",
+    "url": "/oasis/static/media/logmig.e371ea5d.png"
+  },
+  {
+    "revision": "3010af9de0e78372573804641197167f",
+    "url": "/oasis/static/media/logo.3010af9d.png"
+  },
+  {
+    "revision": "e7a15b54b84ff58021f8254d4333bf1e",
+    "url": "/oasis/static/media/logpetit.e7a15b54.png"
+  },
+  {
+    "revision": "456db06ef3078186b08ca5675717caf8",
+    "url": "/oasis/static/media/logran.456db06e.png"
+  },
+  {
+    "revision": "48904b259536a64e26daa0b0fc3fa94b",
+    "url": "/oasis/static/media/marta.48904b25.gif"
+  },
+  {
+    "revision": "3f7060771e5ffeb704917f4fb1c6d33e",
+    "url": "/oasis/static/media/pau.3f706077.gif"
+  }
+]);
