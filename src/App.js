785840c31c494ca708f7a0e4b87330b3e7f14586
adriacabeza <adriacabeza@hotmail.com>
2020-02-07 23:19:32 +0100

    last commits before deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch, BrowserRouter, HashRouter} from 'react-router-dom'
+import {Route, Switch, HashRouter} from 'react-router-dom'
 
 //lazy loading the components
 
