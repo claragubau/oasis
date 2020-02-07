4cf0ea1207d58179a84e9b75cc05e0e5a53772cb
adriacabeza <adriacabeza@hotmail.com>
2020-02-07 22:44:32 +0100

    Updates

---
+++ precache-manifest.67aa76e73cb618b10b0fae063c175f8d.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60a8be95c56e8874b27d75d17e5ae3a",
+    "url": "/oasis/index.html"
+  },
+  {
+    "revision": "54f8837609b0324e73c1",
+    "url": "/oasis/static/css/3.be4727fb.chunk.css"
+  },
+  {
+    "revision": "f22c801c8059c34ec389",
+    "url": "/oasis/static/css/4.25a66862.chunk.css"
+  },
+  {
+    "revision": "6d92a6d7b839f63beec0",
+    "url": "/oasis/static/css/5.596dff72.chunk.css"
+  },
+  {
+    "revision": "309d94049f917c05336e",
+    "url": "/oasis/static/css/6.2b68e173.chunk.css"
+  },
+  {
+    "revision": "04d244a438e4064165be",
+    "url": "/oasis/static/css/7.6c768c0b.chunk.css"
+  },
+  {
+    "revision": "74318333053fdc8cd7ab",
+    "url": "/oasis/static/css/8.2a1ef90b.chunk.css"
+  },
+  {
+    "revision": "410b9e2877c5db0a37ea",
+    "url": "/oasis/static/css/9.919eafe8.chunk.css"
+  },
+  {
+    "revision": "4dd78b961d481d79a44e",
+    "url": "/oasis/static/css/main.9ea0a72c.chunk.css"
+  },
+  {
+    "revision": "c55987afe5252c7d4440",
+    "url": "/oasis/static/js/2.6c492b6a.chunk.js"
+  },
+  {
+    "revision": "54f8837609b0324e73c1",
+    "url": "/oasis/static/js/3.01ae83a7.chunk.js"
+  },
+  {
+    "revision": "f22c801c8059c34ec389",
+    "url": "/oasis/static/js/4.fd85d824.chunk.js"
+  },
+  {
+    "revision": "6d92a6d7b839f63beec0",
+    "url": "/oasis/static/js/5.f7259ce5.chunk.js"
+  },
+  {
+    "revision": "309d94049f917c05336e",
+    "url": "/oasis/static/js/6.70f14774.chunk.js"
+  },
+  {
+    "revision": "04d244a438e4064165be",
+    "url": "/oasis/static/js/7.830a65d7.chunk.js"
+  },
+  {
+    "revision": "74318333053fdc8cd7ab",
+    "url": "/oasis/static/js/8.cfbda210.chunk.js"
+  },
+  {
+    "revision": "410b9e2877c5db0a37ea",
+    "url": "/oasis/static/js/9.d0035043.chunk.js"
+  },
+  {
+    "revision": "4dd78b961d481d79a44e",
+    "url": "/oasis/static/js/main.6c1f4d72.chunk.js"
+  },
+  {
+    "revision": "666a579ee6e22e8bcfb3",
+    "url": "/oasis/static/js/runtime-main.f486a043.js"
+  },
+  {
+    "revision": "a2d95a3b29022010b575cebcd16300f9",
+    "url": "/oasis/static/media/ACTORS.a2d95a3b.jpg"
+  },
+  {
+    "revision": "e5d847d12cbc180adaca8b5e212cc925",
+    "url": "/oasis/static/media/Asap-Regular.e5d847d1.ttf"
+  },
+  {
+    "revision": "e880165377ebffa4028abbea3b2a4f97",
+    "url": "/oasis/static/media/HABADA.e8801653.jpg"
+  },
+  {
+    "revision": "e9b2cda6514835372f9bcee7efc3fdbd",
+    "url": "/oasis/static/media/bequipo.e9b2cda6.gif"
+  },
+  {
+    "revision": "15fd7deb521891f69256db866886bb09",
+    "url": "/oasis/static/media/buke.15fd7deb.gif"
+  },
+  {
+    "revision": "cf7c451748d31cf44a26a4ac9ca344fe",
+    "url": "/oasis/static/media/da.cf7c4517.gif"
+  },
+  {
+    "revision": "ee4e56752f282975a82eaf2ae7b895f9",
+    "url": "/oasis/static/media/fcolaborafinal.ee4e5675.jpg"
+  },
+  {
+    "revision": "0f2b1ae04593767b13c2a858f281f667",
+    "url": "/oasis/static/media/fmenu.0f2b1ae0.jpg"
+  },
+  {
+    "revision": "d3df49f878ccc66a222955cad8880c73",
+    "url": "/oasis/static/media/grevista.d3df49f8.gif"
+  },
+  {
+    "revision": "093d5b5e05ff9024d2f6d2f17c980129",
+    "url": "/oasis/static/media/isi.093d5b5e.gif"
+  },
+  {
+    "revision": "f8372f2ff2ec1ad28b6871b11170452e",
+    "url": "/oasis/static/media/lluis.f8372f2f.gif"
+  },
+  {
+    "revision": "e371ea5d2cde06e4cd1786e0c4528f3e",
+    "url": "/oasis/static/media/logmig.e371ea5d.png"
+  },
+  {
+    "revision": "e7a15b54b84ff58021f8254d4333bf1e",
+    "url": "/oasis/static/media/logpetit.e7a15b54.png"
+  },
+  {
+    "revision": "456db06ef3078186b08ca5675717caf8",
+    "url": "/oasis/static/media/logran.456db06e.png"
+  },
+  {
+    "revision": "17e4f0daaedcde9f0b14b747449d081c",
+    "url": "/oasis/static/media/marta.17e4f0da.gif"
+  },
+  {
+    "revision": "aee4f77efc58e407b9a077108b7157cb",
+    "url": "/oasis/static/media/pau.aee4f77e.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "claragubau.github.io/oasis/precache-manifest.21db6474e9c72d4e12616ea5f915b8bd.js"
+  "/oasis/precache-manifest.67aa76e73cb618b10b0fae063c175f8d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("claragubau.github.io/oasis/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oasis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
