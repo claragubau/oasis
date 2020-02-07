1755cc41378b953ea63c52a7ab1438565667e917
adriacabeza <adriacabeza@hotmail.com>
2020-02-07 22:36:29 +0100

    Updates

---
+++ precache-manifest.f735d3f22056bb95a7d75238f281e333.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29729ea9c5c859cbdca6ea6423e84b77",
+    "url": "oasiselproyecto.com/index.html"
+  },
+  {
+    "revision": "5f0a85c59c3e1cba3f6c",
+    "url": "oasiselproyecto.com/static/css/3.c0fec16f.chunk.css"
+  },
+  {
+    "revision": "a071c5a15d6a8b473267",
+    "url": "oasiselproyecto.com/static/css/4.3f17d99d.chunk.css"
+  },
+  {
+    "revision": "e8441f074838965cac57",
+    "url": "oasiselproyecto.com/static/css/5.b4f3266e.chunk.css"
+  },
+  {
+    "revision": "b995556bd2dcd39d5b33",
+    "url": "oasiselproyecto.com/static/css/6.5f2f1cac.chunk.css"
+  },
+  {
+    "revision": "04d244a438e4064165be",
+    "url": "oasiselproyecto.com/static/css/7.6c768c0b.chunk.css"
+  },
+  {
+    "revision": "863b5eff34d4494ee3ea",
+    "url": "oasiselproyecto.com/static/css/8.fb0a9bff.chunk.css"
+  },
+  {
+    "revision": "a9a8933f26f1bc1bff01",
+    "url": "oasiselproyecto.com/static/css/9.af066f6f.chunk.css"
+  },
+  {
+    "revision": "5ce3d1dce068dce5e3db",
+    "url": "oasiselproyecto.com/static/css/main.4cd95a32.chunk.css"
+  },
+  {
+    "revision": "c55987afe5252c7d4440",
+    "url": "oasiselproyecto.com/static/js/2.6c492b6a.chunk.js"
+  },
+  {
+    "revision": "5f0a85c59c3e1cba3f6c",
+    "url": "oasiselproyecto.com/static/js/3.059928a4.chunk.js"
+  },
+  {
+    "revision": "a071c5a15d6a8b473267",
+    "url": "oasiselproyecto.com/static/js/4.11a75a02.chunk.js"
+  },
+  {
+    "revision": "e8441f074838965cac57",
+    "url": "oasiselproyecto.com/static/js/5.a4560c04.chunk.js"
+  },
+  {
+    "revision": "b995556bd2dcd39d5b33",
+    "url": "oasiselproyecto.com/static/js/6.c33f7606.chunk.js"
+  },
+  {
+    "revision": "04d244a438e4064165be",
+    "url": "oasiselproyecto.com/static/js/7.830a65d7.chunk.js"
+  },
+  {
+    "revision": "863b5eff34d4494ee3ea",
+    "url": "oasiselproyecto.com/static/js/8.3cd5da4a.chunk.js"
+  },
+  {
+    "revision": "a9a8933f26f1bc1bff01",
+    "url": "oasiselproyecto.com/static/js/9.12245483.chunk.js"
+  },
+  {
+    "revision": "5ce3d1dce068dce5e3db",
+    "url": "oasiselproyecto.com/static/js/main.fa1806a2.chunk.js"
+  },
+  {
+    "revision": "be5ba73b186faddfd0f8",
+    "url": "oasiselproyecto.com/static/js/runtime-main.8c179a0a.js"
+  },
+  {
+    "revision": "a2d95a3b29022010b575cebcd16300f9",
+    "url": "oasiselproyecto.com/static/media/ACTORS.a2d95a3b.jpg"
+  },
+  {
+    "revision": "e5d847d12cbc180adaca8b5e212cc925",
+    "url": "oasiselproyecto.com/static/media/Asap-Regular.e5d847d1.ttf"
+  },
+  {
+    "revision": "e880165377ebffa4028abbea3b2a4f97",
+    "url": "oasiselproyecto.com/static/media/HABADA.e8801653.jpg"
+  },
+  {
+    "revision": "e9b2cda6514835372f9bcee7efc3fdbd",
+    "url": "oasiselproyecto.com/static/media/bequipo.e9b2cda6.gif"
+  },
+  {
+    "revision": "15fd7deb521891f69256db866886bb09",
+    "url": "oasiselproyecto.com/static/media/buke.15fd7deb.gif"
+  },
+  {
+    "revision": "cf7c451748d31cf44a26a4ac9ca344fe",
+    "url": "oasiselproyecto.com/static/media/da.cf7c4517.gif"
+  },
+  {
+    "revision": "ee4e56752f282975a82eaf2ae7b895f9",
+    "url": "oasiselproyecto.com/static/media/fcolaborafinal.ee4e5675.jpg"
+  },
+  {
+    "revision": "0f2b1ae04593767b13c2a858f281f667",
+    "url": "oasiselproyecto.com/static/media/fmenu.0f2b1ae0.jpg"
+  },
+  {
+    "revision": "d3df49f878ccc66a222955cad8880c73",
+    "url": "oasiselproyecto.com/static/media/grevista.d3df49f8.gif"
+  },
+  {
+    "revision": "093d5b5e05ff9024d2f6d2f17c980129",
+    "url": "oasiselproyecto.com/static/media/isi.093d5b5e.gif"
+  },
+  {
+    "revision": "f8372f2ff2ec1ad28b6871b11170452e",
+    "url": "oasiselproyecto.com/static/media/lluis.f8372f2f.gif"
+  },
+  {
+    "revision": "e371ea5d2cde06e4cd1786e0c4528f3e",
+    "url": "oasiselproyecto.com/static/media/logmig.e371ea5d.png"
+  },
+  {
+    "revision": "e7a15b54b84ff58021f8254d4333bf1e",
+    "url": "oasiselproyecto.com/static/media/logpetit.e7a15b54.png"
+  },
+  {
+    "revision": "456db06ef3078186b08ca5675717caf8",
+    "url": "oasiselproyecto.com/static/media/logran.456db06e.png"
+  },
+  {
+    "revision": "17e4f0daaedcde9f0b14b747449d081c",
+    "url": "oasiselproyecto.com/static/media/marta.17e4f0da.gif"
+  },
+  {
+    "revision": "aee4f77efc58e407b9a077108b7157cb",
+    "url": "oasiselproyecto.com/static/media/pau.aee4f77e.gif"
+  }
+]);
